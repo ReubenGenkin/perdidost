995967bbdbe3db72161541f6d6f717c84661c4c8
Reuben <rleibgenkin@gmail.com>
2021-06-28 18:14:02 -0400

    added more pathing to api

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json);
 
@@ -14,23 +14,25 @@ app.use(express.json);
 
 app.get('/notes', (req, res) => {
 
-res.sendFile(path.join(__dirname, '../public/notes.html'))
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
 
 })
 
 app.get('*', (req, res) => {
 
     res.sendFile(path.join(__dirname, '../public/index.html'))
-    
-    })
-
-app.get('/notes', (req, res) => {
-
-res.sendFile(path.join(__dirname, '../public/notes.html'))
 
 })
 
+app.get('/api/notes', (req, res) => {
 
+    fs.readFile(path.join(__dirname, '../db/db.json'));
+    res.json(data);
+});
+
+app.post('/api/notes' (req, res) => {
+
+})
 
 
 app.listen(PORT, () => {
